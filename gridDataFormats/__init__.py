d8aec9466b4a246f784c7e3bbc4cbb86519c0772
Oliver Beckstein <orbeckst@gmail.com>
2009-05-25 16:55:41 +0000

    add grid data formats to the master package
    git-svn-id: svn+ssh://gonzo.med.jhmi.edu/scratch/svn/woolf_repository/users/oliver/Hop@3481 df5ba8eb-4b0b-0410-8c14-c10f23b0129c

---
+++ gridDataFormats/__init__.py
@@ -0,0 +1,45 @@
+# $Id$
+# Copyright (c) 2007, 2008 Oliver Beckstein <orbeckst@gmail.com>
+# Released under the GNU Public License.
+
+"""gridDataFormat module
+
+Copyright (c) 2007, 2008 Oliver Beckstein <orbeckst@gmail.com>
+Released under the GNU Public License.
+
+This module contains classes that allow importing and exporting of
+simple gridded data, A grid is an N-dimensional array that represents
+a discrete mesh over a region of space. The array axes are taken to be
+parallel to the cartesian axes of this space. Together with this array
+we also store the edges, which are are (essentially) the cartesian
+coordinates of the intersections of the grid (mesh) lines on the
+axes. In this way the grid is anchored in space.
+
+The Grid class acts as a universal constructor for specific formats.
+
+ g = Grid(**kwargs)           # construct
+ g.export(filename, format)   # export to the desire format
+
+Some formats can also be read:
+
+ g = Grid()                   # make an empty Grid
+ g.load(filename)             # populate with data from filename
+
+See the doc string for Grid for details on **kwargs.
+
+Formats:
+
+   OpenDX        IBM's Data Explorer, http://www.opendx.org/
+   gOpenMol      http://www.csc.fi/gopenmol/
+   pickle        python pickle file
+"""
+
+__all__ =  ['core','OpenDX','gOpenMol']
+
+import warnings
+
+class gridDataWarning(Warning):
+    """Warns of a problem specific to the gridData module."""
+    pass
+
+from core import Grid
